d03f3fc7a3beaef06528128d3da012313e50a40c
EgrSuleiko <Egor.Suleiko@yandex.by>
2025-05-20 18:58:27 +0300

    implemented new templates and views

--- analyze/urls.py
+++ analyze/urls.py
@@ -5,8 +5,11 @@ app_name = 'analyze'
 urlpatterns = [
     path('', views.index, name='index'),
     path('gallery', views.gallery, name='gallery'),
+    path('add_to_cart', views.add_to_cart, name='add_to_cart'),
+    path('text', views.get_text, name='text'),
     path('upload/', views.FileUploadView.as_view(), name='upload'),
     path('upload/success', views.upload_success, name='upload_success'),
     path('cart', views.cart, name='cart'),
+    path('cart_payment', views.cart_payment, name='cart_payment'),
     path('prices', views.prices, name='prices'),
 ]
--- analyze/views.py
+++ analyze/views.py
@@ -6,7 +6,7 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
-from analyze.models import Doc, Price, UserToDoc
+from analyze.models import Doc, Price, UserToDoc, Cart
 from analyze.forms import FileUploadForm
 
 from django_project import settings
@@ -37,7 +37,21 @@ def gallery(request):
         if doc['id'] in user_docs_server_id:
             displayed_docs.append(doc)
 
+    paid_docs_server_id = set(
+        Doc.objects.filter(cart__payment=True)
+        .values_list('server_id', flat=True)
+        .distinct()
+    )
+
+    in_cart_docs_server_id = set(
+        Doc.objects.filter(cart__payment=False)
+        .values_list('server_id', flat=True)
+        .distinct()
+    )
+
     for doc in displayed_docs:
+        doc['paid'] = doc['id'] in paid_docs_server_id
+        doc['in_cart'] = doc['id'] in in_cart_docs_server_id
         doc['image_url'] = f'{settings.PHOTO_SERVICE_URL}/documents/file/{doc['path']}'
         doc['date'] = datetime.strptime(doc['date'], '%Y-%m-%dT%H:%M:%S.%f')
 
@@ -90,11 +104,69 @@ class FileUploadView(LoginRequiredMixin, View):
         return redirect('analyze:upload_success')
 
 
+def add_to_cart(request):
+    server_doc_id = request.POST.get('server_doc_id')
+
+    doc = Doc.objects.get(server_id=server_doc_id)
+    price_for_doc_type = Price.objects.get(file_type=doc.file_type)
+    new_cart = Cart(order_price=price_for_doc_type.price * doc.size / 1024, user_id=request.user.id, doc_id=doc.id)
+    new_cart.save()
+
+    return redirect('analyze:gallery')
+
+
+def cart_payment(request):
+    template_name = 'analyze/cart.html'
+    cart_id = request.POST.get('cart_id')
+
+    cart = Cart.objects.filter(id=cart_id)
+    cart.update(payment=True)
+
+    is_lang_rus = request.POST.get('rus_lang')
+
+    server_doc_id = cart.select_related('doc').values_list('doc__server_id', flat=True)[0]
+    try:
+        response = requests.post(
+            url=f'{settings.PHOTO_SERVICE_URL}/documents/analyze/{server_doc_id}',
+            params={'language': 'rus' if is_lang_rus else 'eng'},
+            timeout=30
+        )
+        response.raise_for_status()
+
+    except requests.exceptions.RequestException as e:
+        return render(request, template_name, {'error': f'Ошибка отправки файла на анализ {str(e)}'})
+
+    return redirect('analyze:cart')
+
+
+def cart(request):
+    cart_items = Cart.objects.filter(user_id=request.user.id)
+    context = {'cart': cart_items}
+
+    return render(request, 'analyze/cart.html', context)
+
+
+def get_text(request):
+    template_name = 'analyze/text.html'
+    doc_id = request.POST.get('doc_id')
+
+    context = {'doc_id': doc_id}
+
+    try:
+        response = requests.get(
+            url=f'{settings.PHOTO_SERVICE_URL}/documents/get_text/{doc_id}',
+            timeout=30
+        )
+        response.raise_for_status()
+
+        context['text'] = response.content.decode('utf-8')
+
+    except requests.exceptions.RequestException as e:
+        return render(request, template_name, {'error': f'Ошибка загрузки текста {str(e)}'})
+
+    return render(request, template_name, context)
+
+
 @login_required()
 def upload_success(request):
     return render(request, 'analyze/upload_success.html')
-
-
-@login_required()
-def cart(request):
-    return render(request, 'analyze/cart.html')
